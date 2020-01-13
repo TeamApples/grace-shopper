9b38ca8625f880fa4c939c4f7d5311f6f26cfd69
John Cho <chojh92@gmail.com>
2020-01-13 16:32:19 -0500

    order model added

---
+++ server/db/models/order.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize')
+const db = require('../db.js')
+
+const Order = db.define('order', {
+  status: {
+    type: Sequelize.ENUM('processing', 'shipped', 'cancelled', 'completed'),
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  shippingMethod: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  paymentMethod: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  }
+})
+
+module.exports = Order
