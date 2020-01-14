6d973273ac3b9547a21cafdeccfa512681c2e9e6
Roy-Tessler <23tessler@gmail.com>
2020-01-14 15:41:11 -0500

    change

---
+++ client/components/AllProducts.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import {gotAllProducts} from '../store/product'
+import {connect} from 'react-redux'
+
+class AllProducts extends Component {
+  componentDidMount() {
+    this.props.onLoadProducts()
+  }
+
+  render() {
+    const products = this.props.products
+    return (
+      <div className="all-products">
+        <h2>Shop All Products</h2>
+        <div className="product-view">
+          {Array.isArray(products) &&
+            products.map(product => {
+              return (
+                <ul key={product.id}>
+                  <li>
+                    <Link to={'/products/' + product.id}>{product.name}</Link>
+                    <img src={product.image} />
+                    <h2>${product.price}</h2>
+                    <p>{product.description}</p>
+                    <button type="button">Buy Now</button>
+                  </li>
+                </ul>
+              )
+            })}
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = function(state) {
+  return {
+    products: state.products
+  }
+}
+
+const mapDispatchToProps = function(dispatch) {
+  return {
+    onLoadProducts: function() {
+      const action = gotAllProducts()
+      dispatch(action)
+    }
+  }
+}
+
+const AllProductsContainer = connect(mapStateToProps, mapDispatchToProps)(
+  AllProducts
+)
+
+export default AllProductsContainer
