9cf777e6ba1e2d7b6994f8632e155b91c2bcf58c
Roy-Tessler <23tessler@gmail.com>
2020-01-16 18:10:47 -0500

    started actions for removing item from cart

--- client/store/cart.js
+++ client/store/cart.js
@@ -2,10 +2,11 @@ import Axios from 'axios'
 
 const ADD_TO_CART = 'ADD_TO_CART'
 const GET_CART = 'GET_CART'
+const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
 
 const cart = []
 
-export const addProductToCart = function(product, quantity) {
+export const addProductToCart = function(product) {
   return {
     type: ADD_TO_CART,
     product: product
@@ -16,6 +17,21 @@ export const getCart = function(cart) {
   return {
     type: GET_CART,
     cart
+  }
+}
+
+const deleteProduct = function(productId, orderId) {
+  return {
+    type: REMOVE_FROM_CART,
+    productId: Number(productId),
+    orderId: Number(orderId)
+  }
+}
+
+export const deleteProductThunk = function(product, order) {
+  return async dispatch => {
+    await Axios.delete(`/api/orders/${order.id}/${product.id}`)
+    dispatch(deleteProduct(product.id, order.id))
   }
 }
 
@@ -35,15 +51,17 @@ export const gotCart = userId => {
 
 export const cartReducer = (state = cart, action) => {
   switch (action.type) {
-    case ADD_TO_CART:
-      let productIdMap = new Set()
-      cart.forEach(product => {
-        productIdMap.add(product.id)
-      })
-      if (productIdMap.has(action.product.id))
-        if (action.product.id) return [...state, action.product]
+    // case ADD_TO_CART:
+    // let productIdMap = new Set()
+    // cart.forEach(product => {
+    //   productIdMap.add(product.id)
+    // })
+    // if (productIdMap.has(action.product.id))
+    //   if (action.product.id) return [...state, action.product]
     case GET_CART:
       return action.cart
+    case REMOVE_FROM_CART:
+      return state.filter(product => product.id !== action.productId)
     default:
       return state
   }
