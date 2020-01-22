1b1cea44131ac09b038310c85c674d44f57c3f04
Christina Kang <cykang90@gmail.com>
2020-01-22 17:29:20 -0500

    working on OAuth

--- server/auth/google.js
+++ server/auth/google.js
@@ -48,10 +48,7 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
 
   passport.use(strategy)
 
-  router.get(
-    '/',
-    passport.authenticate('google', {scope: ['email', 'profile']})
-  )
+  router.get('/', passport.authenticate('google', {scope: 'email'}))
 
   router.get(
     '/callback',
